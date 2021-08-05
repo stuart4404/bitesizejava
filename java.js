b2d8fa49bbaa875192947663116ad20541e128b7
stuart tichener <76698984+stuart4404@users.noreply.github.com>
2021-08-05 10:47:30 +0100

    Add files via upload

---
+++ java.js
@@ -0,0 +1,18 @@
+let a = 1 + 1
+console.log(a)
+
+// some simple else if statements
+let direction = 'left';
+direction = 'right';
+direction = 'up';
+
+if (direction =='left') {
+    console.log("red route");
+}
+
+else if (direction =='right') {
+    console.log("blue route");
+}
+else {
+    console.log("crash");
+}
